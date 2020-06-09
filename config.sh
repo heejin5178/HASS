5c61e32c92c2930f4e030f77b27cbdf82c9adfdc
heejin <heejin5178@gmail.com>
2020-06-09 07:32:44 +0000

    final update for config.sh

--- config.sh
+++ config.sh
@@ -3,39 +3,41 @@
 server=$1
 git pull origin master
 cd mongo-r3.6.18
-if [ $server == 3 ] 
+if [ $server == "3" ] 
 then
 #mongos, mongod - apple
 buildscripts/scons.py MONGO_VERSION=3.6.18 mongos
 buildscripts/scons.py MONGO_VERSION=3.6.18 mongod
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_apple.conf -fork
-sudo ./mongod --configsvr -f /home/heejin/config/mongodb_config.conf -fork
-sudo ./mongos -f /home/heejin/config/mongos.conf --fork
-
-elif [ $sever == 4 ]
+sudo ./mongod  -f /home/heejin/config/mongodb_apple.conf --fork;
+echo "apple shard on"
+sudo ./mongod --configsvr -f /home/heejin/config/mongodb_config.conf --fork;
+echo "config on"
+sudo ./mongos -f /home/heejin/config/mongos.conf;
+echo "mongos on"
+elif [ $sever == "4" ]
 then
 #mongod - apple,banana
 buildscripts/scons.py MONGO_VERSION=3.6.18 mongod
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_apple.conf -fork
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_banana.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_apple.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_banana.conf -fork
 sudo ./mongod --configsvr -f /home/heejin/config/mongodb_config.conf -fork
-elif [ $server == 5 ]
+elif [ $server == "5" ]
 then
 #mongod - apple,banana,mango
 buildscripts/scons.py MONGO_VERSION=3.6.18 mongod
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_apple.conf -fork
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_banana.conf -fork
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_mango.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_apple.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_banana.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_mango.conf -fork
 sudo ./mongod --configsvr -f /home/heejin/config/mongodb_config.conf -fork
-elif [ $server == 6 ]
+elif [ $server == "6" ]
 then
 #mongod - banana,mango
 buildscripts/scons.py MONGO_VERSION=3.6.18 mongod
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_mango.conf -fork
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_banana.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_mango.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_banana.conf -fork
 else
 #mongod - mango
 buildscripts/scons.py MONGO_VERSION=3.6.18 mongod
-sudo ./mongod --shardsvr -f /home/heejin/config/mongodb_mango.conf -fork
+sudo ./mongod  -f /home/heejin/config/mongodb_mango.conf -fork
 fi
 
