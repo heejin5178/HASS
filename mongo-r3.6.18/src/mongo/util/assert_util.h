546af4ae8f3d9bc4d2cbb52bbbe3d1e1d0d64e19
heejin <heejin5178@gmail.com>
2020-06-17 08:29:53 +0000

    invariant rollback

--- mongo-r3.6.18/src/mongo/util/assert_util.h
+++ mongo-r3.6.18/src/mongo/util/assert_util.h
@@ -401,14 +401,11 @@ inline void massertStatusOKWithLocation(const Status& status, const char* file, 
 #define invariantOK MONGO_invariantOK
 #define MONGO_invariantOK(expression)                                                         \
     do {  \
-   	} while (false)
-/*                                                                                     \
         const ::mongo::Status _invariantOK_status = expression;                               \
         if (MONGO_unlikely(!_invariantOK_status.isOK())) {                                    \
             ::mongo::invariantOKFailed(#expression, _invariantOK_status, __FILE__, __LINE__); \
-
         }                                                                                     \
- */
+   	} while (false)
 	
 
 #define dassertOK MONGO_dassertOK
